c934d8f078c3bf6d96ebb1c49e3df9dba1e0d25e
DEVJAYDEEP <jaydeepkumar2006@gmail.com>
2021-10-18 02:59:42 +0000

    mojito: AOSPK bringup

--- aosp_mojito.mk
+++ aosp_mojito.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some LineageOS stuffs
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
+# Inherit some Kraken stuffs
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_mojito
+PRODUCT_NAME := aosp_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
@@ -30,9 +30,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys"
 
-# #Gapps
-# TARGET_GAPPS_ARCH = arm64
-#
-# # OFFICIAL SPICEOS Build
-#
-# SPICEOS_BUILDTYPE = OFFICIAL
+
